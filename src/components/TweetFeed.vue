1d4695ea4e4fe53cbb315801b2acdf78cfd75f98
funiel <d.wetzelhuetter@gmail.com>
2017-12-17 14:48:18 +0100

    update to grid system (temp) (2)

---
+++ src/components/TweetFeed.vue
@@ -0,0 +1,26 @@
+<<template>
+    <div>
+        <Tweet :id="'865986101437247490'"></Tweet>
+        <Tweet :id="'865986101437247490'"></Tweet>
+        <Tweet :id="'865986101437247490'"></Tweet>
+        <Tweet :id="'865986101437247490'"></Tweet>
+        <Tweet :id="'865986101437247490'"></Tweet>
+    </div>
+</template>
+
+<script>
+    import {
+        Tweet,
+        Moment
+    } from 'vue-tweet-embed'
+    
+    export default {
+        components: {
+            Tweet
+        }
+    }
+</script>
+
+<style>
+    
+</style>
---
+++ src/views/Category.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="component-wrapper">
+        <h1>{{ category }}</h1>
+        <hr>
+        <CategoryList class="" :categoryToRender="category" :showText="true" :showAuthor="true" :mode="'list-'" :imageStyle="'next-to-article'">
+        </CategoryList>
+    
+    </div>
+    
+</template>
+
+<script>
+    import apiMixin from '../mixins/api.js'
+    import * as apiMuts from '../api/mutation-types.js'
+    import * as apiActs from '../api/action-types.js'
+    import CategoryList from '../components/ListArticles'
+    
+    export default {
+        components: {
+            CategoryList
+        },
+        mixins: [apiMixin],
+    
+        props: {
+            category: {
+                type: String,
+                default: 'Default Category'
+            }
+        },
+        mounted() {},
+    
+        computed: {
+        }
+    
+    
+    }
+</script>
+
+<style>
+    
+</style>

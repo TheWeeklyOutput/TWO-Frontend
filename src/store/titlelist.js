cea0f5dfa3fa1bec1eef20ae6a470a106e87e0c4
funiel <d.wetzelhuetter@gmail.com>
2018-01-14 15:00:35 +0100

    Twitter Bar now with recents (3)

---
+++ src/store/titlelist.js
@@ -0,0 +1,15 @@
+import EntertainmentTitle from '../assets/svg/titles/Entertainment.svg'
+import HealthTitle from '../assets/svg/titles/Health.svg'
+import SportsTitle from '../assets/svg/titles/Sports.svg'
+import HighlightsTitle from '../assets/svg/titles/Highlights.svg'
+import TechTitle from '../assets/svg/titles/Tech.svg'
+import PoliticsTitle from '../assets/svg/titles/Politics.svg'
+
+export const TitleList = {
+    Entertainment: EntertainmentTitle,
+    Health: HealthTitle,
+    Sports: SportsTitle,
+    Highlights: HighlightsTitle,
+    Tech: TechTitle,
+    Politics: PoliticsTitle
+}
